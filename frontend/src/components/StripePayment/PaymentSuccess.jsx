757c019ee09165cf9cb7cf003ca1d45da809cae5
TEJANAIK <134024083+TejaNaik15@users.noreply.github.com>
2025-10-17 12:14:19 +0530

    Initial commit - NovaGen AI Content Generator

--- frontend/src/components/StripePayment/PaymentSuccess.jsx
+++ frontend/src/components/StripePayment/PaymentSuccess.jsx
@@ -6,10 +6,10 @@ import { useQuery } from "@tanstack/react-query";
 import { verifyPaymentAPI } from "../../apis/stripePayment/stripePayment";
 
 const PaymentSuccess = () => {
-  //get the params
+  
   const [searchParams] = useSearchParams();
   const paymentIntentID = searchParams.get("payment_intent");
-  //UseQuery
+  
 
   const { isLoading, isError, data, isPending, isSuccess } = useQuery({
     queryFn: () => verifyPaymentAPI(paymentIntentID),
@@ -18,7 +18,7 @@ const PaymentSuccess = () => {
   return (
     <div className="max-w-lg mx-auto my-10 p-6 bg-white shadow-md rounded-lg">
       {isLoading ? (
-        // Simulate loading state
+        
         <div className="flex flex-col items-center justify-center">
           <FaSpinner className="animate-spin text-4xl text-blue-500 mb-3" />
           <p className="text-lg text-gray-600">
@@ -26,14 +26,14 @@ const PaymentSuccess = () => {
           </p>
         </div>
       ) : isError ? (
-        // Simulate error state
+        
         <div className="text-center text-red-500">
           <FaTimesCircle className="text-5xl mb-3" />
           <p className="text-xl">Payment verification failed</p>
           <p>An error occurred</p>
         </div>
       ) : (
-        // Simulate success state
+        
         <div className="text-center text-green-500">
           <FaCheckCircle className="text-5xl mb-3" />
           <h1 className="text-2xl font-bold mb-3">Payment Successful</h1>
