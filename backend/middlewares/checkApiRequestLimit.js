1072bf3c65b81a5dc4c1ec69c8b3279c96904621
TEJANAIK <134024083+TejaNaik15@users.noreply.github.com>
2025-10-17 12:24:03 +0530

    Initial commit - NovaGen AI Content Generator

--- backend/middlewares/checkApiRequestLimit.js
+++ backend/middlewares/checkApiRequestLimit.js
@@ -5,14 +5,14 @@ const checkApiRequestLimit = asyncHandler(async (req, res, next) => {
   if (!req.user) {
     return res.status(401).json({ message: "Not authorized" });
   }
-  //Find the user
+  
   const user = await User.findById(req?.user?.id);
   if (!user) {
     return res.status(404).json({ message: "User not found" });
   }
   let requestLimit = user?.monthlyRequestCount || 0;
   
-  // Check if user has reached their limit
+  
   if (user?.apiRequestCount >= requestLimit) {
     return res.status(429).json({ 
       message: "API Request limit reached",
@@ -22,7 +22,7 @@ const checkApiRequestLimit = asyncHandler(async (req, res, next) => {
     });
   }
 
-  // If we get here, the user hasn't reached their limit
+  
   next();
 });
 
