a49c0be16e983c00dc5842e8f2fbd33ee92ed2c2
TEJANAIK <134024083+TejaNaik15@users.noreply.github.com>
2025-10-17 12:23:33 +0530

    Initial commit - NovaGen AI Content Generator

--- backend/middlewares/isAuthenticated.js
+++ backend/middlewares/isAuthenticated.js
@@ -2,16 +2,16 @@ const asyncHandler = require("express-async-handler");
 const jwt = require("jsonwebtoken");
 const User = require("../models/User");
 
-//----IsAuthenticated middleware
+
 const isAuthenticated = asyncHandler(async (req, res, next) => {
   const token = req.cookies.token;
   if (!token) {
     return res.status(401).json({ message: "Not authorized, no token" });
   }
   try {
-    // Verify the token
+    
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    // Add the user to the req obj
+    
     req.user = await User.findById(decoded?.id).select("-password");
     if (!req.user) {
       return res.status(401).json({ message: "Not authorized, user not found" });
